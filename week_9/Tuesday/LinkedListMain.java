2cb21311f63c2a193d9e38c5b2563d36cc18fa0c
XPandora <330281987@qq.com>
2023-11-29 11:17:21 -0800

    complete version of LinkedList

--- week_9/Tuesday/LinkedListMain.java
+++ week_9/Tuesday/LinkedListMain.java
@@ -1,12 +1,30 @@
 class LinkedList {
   private Node head;
+
   public LinkedList() {
     head = null;
   }
 
   public void add(int data) {
-    // TODO: add a new node to the linked list
-    
+    // step 1: initialize an instance of Node
+    Node n = new Node(data);
+
+    // step 2: append this Node n to the tail of this linked list
+    // corner case: no nodes in this linked list
+    if (head == null) {
+      head = n;
+    } else {
+      // means we do have some nodes inside this linked list
+      // we have to find the tail of this list
+      Node node = head;
+      while (node.next != null) {
+        node = node.next;
+      }
+
+      // after the while loop, node is the tail of this linked list
+      // because its next is null
+      node.next = n;
+    }
   }
 
   public void print() {
